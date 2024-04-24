5457e5d4e2b812344f1a86a1451ae6b7226c98af
TIleeminki <minki.lee@trialinformatics.com>
2024-04-24 15:03:42 +0900

    fitst commit

---
+++ src/main/java/com/trialinformatics/docsauth/DocsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.trialinformatics.docsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocsAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trialinformatics/docsauth/DocsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.trialinformatics.docsauth;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DocsAuthApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
